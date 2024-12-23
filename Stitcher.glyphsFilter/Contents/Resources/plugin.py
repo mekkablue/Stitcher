e7a239a41023f1b419d495ff588832c737cdcaec
mekkablue <rainer@schriftlabor.at>
2024-12-23 16:09:22 -0300

    Allow rotation by adding an orientation anchor to affected components

--- Stitcher.glyphsFilter/Contents/Resources/plugin.py
+++ Stitcher.glyphsFilter/Contents/Resources/plugin.py
@@ -18,23 +18,15 @@ from __future__ import division, print_function, unicode_literals
 import objc
 from GlyphsApp import *
 from GlyphsApp.plugins import *
-from math import hypot
+from math import hypot, atan2
+from AppKit import NSAffineTransform
 
 @objc.python_method
-def deleteAllComponents( thisLayer ):
+def deleteAllComponents(thisLayer):
 	try:
-		# print("-- Deleting %i existing components." % ( len(thisLayer.components) )) #DEBUG
-		
-		try:
-			# GLYPHS 3
-			for i in reversed(range(len(thisLayer.shapes))):
-				if type(thisLayer.shapes[i]) == GSComponent:
-					del thisLayer.shapes[i]
-		except:
-			# GLYPHS 2
-			while len(thisLayer.components) > 0:
-				del thisLayer.components[0]
-
+		for i in reversed(range(len(thisLayer.shapes))):
+			if type(thisLayer.shapes[i]) == GSComponent:
+				del thisLayer.shapes[i]
 		return True
 		
 	except Exception as e:
@@ -42,8 +34,9 @@ def deleteAllComponents( thisLayer ):
 		print(traceback.format_exc())
 		return False
 
+
 @objc.python_method
-def bezier( A, B, C, D, t ):
+def bezier(A, B, C, D, t):
 	x1, y1 = A.x, A.y
 	x2, y2 = B.x, B.y
 	x3, y3 = C.x, C.y
@@ -54,12 +47,14 @@ def bezier( A, B, C, D, t ):
 
 	return x, y
 
-@objc.python_method
-def distance( node1, node2 ):
-	return hypot( node1.x - node2.x, node1.y - node2.y )
 
 @objc.python_method
-def segmentsForPath( p ):
+def distance(node1, node2):
+	return hypot(node1.x - node2.x, node1.y - node2.y)
+
+
+@objc.python_method
+def segmentsForPath(p):
 	segments=[]
 	currentSegment=[]
 	pathlength = len(p.nodes)
@@ -70,40 +65,84 @@ def segmentsForPath( p ):
 			if not offset in (2,4):
 				firstPoint = p.nodes[(i-offset)%pathlength].position
 				currentSegment.insert(0, firstPoint)
-			segments.append( tuple(currentSegment) )
+			segments.append(tuple(currentSegment))
 			currentSegment=[]
 	return segments
-	
+
 
 @objc.python_method
-def getFineGrainPointsForPath( thisPath, distanceBetweenDots, precision=10 ):
+def getOrientationAngle(originPoint, orientationPoint):
+	dx = orientationPoint.x - originPoint.x
+	dy = orientationPoint.y - originPoint.y
+	return atan2(dy, dx)
+
+
+@objc.python_method
+def createTransformMatrix(origin, angle):
+	transform = NSAffineTransform.transform()
+	transform.translateXBy_yBy_(origin.x, origin.y)
+	transform.rotateByRadians_(angle)
+	transform.translateXBy_yBy_(-origin.x, -origin.y)
+	return transform.transformStruct()
+
+
+@objc.python_method
+def getTangentAngle(p1, p2, p3=None, p4=None, t=0):
+	# Line segment case
+	if p3 is None and p4 is None:
+		dx = p2[0] - p1[0]
+		dy = p2[1] - p1[1]
+		return atan2(dy, dx)
+	
+	# Cubic Bezier case
+	# First derivative of cubic Bezier gives tangent direction
+	t = max(0, min(1, t))  # Clamp t between 0 and 1
+	
+	# Derivative coefficients
+	mt = 1 - t
+	dx = 3 * mt**2 * (p2[0] - p1[0]) + \
+		 6 * mt * t * (p3[0] - p2[0]) + \
+		 3 * t**2 * (p4[0] - p3[0])
+		 
+	dy = 3 * mt**2 * (p2[1] - p1[1]) + \
+		 6 * mt * t * (p3[1] - p2[1]) + \
+		 3 * t**2 * (p4[1] - p3[1])
+	
+	return atan2(dy, dx)
+
+
+@objc.python_method
+def getFineGrainPointsForPath(thisPath, distanceBetweenDots, precision=10):
 	try:
-		layerCoords = [ ]
-		# pathSegments = segmentsForPath( thisPath )
+		layerCoords = []
+		tangentAngles = []
+		# pathSegments = segmentsForPath(thisPath)
 		pathSegments = thisPath.segments
 		
 		for thisSegment in pathSegments:
 			# gsPathSegment = GSPathSegment(*thisSegment) # convert to GSPathSegment for convenience methods
 			segmentLength = thisSegment.length()
 			
-			if len( thisSegment ) == 2:
+			if len(thisSegment) == 2:
 				# straight line:
 				beginPoint = thisSegment[0]
 				endPoint   = thisSegment[1]
+				tangentAngle = getTangentAngle(beginPoint, endPoint)
 				dotsPerSegment = int(segmentLength/distanceBetweenDots*11)
-				for i in range( dotsPerSegment ):
-					x = float( endPoint.x * i ) / dotsPerSegment + float( beginPoint.x * ( dotsPerSegment-i ) ) / dotsPerSegment
-					y = float( endPoint.y * i ) / dotsPerSegment + float( beginPoint.y * ( dotsPerSegment-i ) ) / dotsPerSegment
-					layerCoords += [ NSPoint( x, y ) ]
+				for i in range(dotsPerSegment):
+					x = float(endPoint.x * i) / dotsPerSegment + float(beginPoint.x * (dotsPerSegment-i)) / dotsPerSegment
+					y = float(endPoint.y * i) / dotsPerSegment + float(beginPoint.y * (dotsPerSegment-i)) / dotsPerSegment
+					layerCoords.append(NSPoint(x, y))
+					tangentAngles.append(tangentAngle)
 				
-			elif len( thisSegment ) == 4:
+			elif len(thisSegment) == 4:
 				# curved segment:
 				bezierPointA = thisSegment[0]
 				bezierPointB = thisSegment[1]
 				bezierPointC = thisSegment[2]
 				bezierPointD = thisSegment[3]
 				
-				# segmentLength = distance( bezierPointA, bezierPointB ) + distance( bezierPointB, bezierPointC ) + distance( bezierPointC, bezierPointD ) # very rough approximation, up to 11% too long
+				# segmentLength = distance(bezierPointA, bezierPointB) + distance(bezierPointB, bezierPointC) + distance(bezierPointC, bezierPointD) # very rough approximation, up to 11% too long
 				
 				previousPoint = bezierPointA
 				for i in range(precision):
@@ -112,42 +151,47 @@ def getFineGrainPointsForPath( thisPath, distanceBetweenDots, precision=10 ):
 					# length += distance(previousPoint,currentPoint)
 					previousPoint = currentPoint
 				
-				dotsPerSegment = int( ( segmentLength / distanceBetweenDots ) * 10 )
+				dotsPerSegment = int((segmentLength / distanceBetweenDots) * 10)
 			
-				for i in range( 1, dotsPerSegment ):
-					t = float( i ) / float( dotsPerSegment )
-					x, y = bezier( bezierPointA, bezierPointB, bezierPointC, bezierPointD, t )
-					layerCoords += [ NSPoint( x, y ) ]
+				for i in range(1, dotsPerSegment):
+					t = float(i) / float(dotsPerSegment)
+					x, y = bezier(bezierPointA, bezierPointB, bezierPointC, bezierPointD, t)
+					layerCoords.append(NSPoint(x, y))
+					tangentAngles.append(getTangentAngle(bezierPointA, bezierPointB, bezierPointC, bezierPointD, t))
 			
 				layerCoords.append(NSPoint(bezierPointD.x, bezierPointD.y))
-	
-		return layerCoords
+				tangentAngles.append(getTangentAngle(bezierPointA, bezierPointB, bezierPointC, bezierPointD, 1))
+				
+		return layerCoords, tangentAngles
 	except Exception as e:
 		print("Stitcher Error in getFineGrainPointsForPath():\n%s"%e)
 		import traceback
 		print(traceback.format_exc())
 
+
 @objc.python_method
-def interpolatePointPos( p1, p2, factor ):
+def interpolatePointPos(p1, p2, factor):
 	factor = factor % 1.0
 	x = p1.x * factor + p2.x * (1.0-factor)
 	y = p1.y * factor + p2.y * (1.0-factor)
 	return NSPoint(x,y)
 
+
 @objc.python_method
-def dotCoordsOnPath( thisPath, distanceBetweenDots, balanceOverCompletePath=False ):
+def dotCoordsOnPath(thisPath, distanceBetweenDots, balanceOverCompletePath=False):
 	try:
 		firstPoint = thisPath.nodes[0]
 		if firstPoint.type==OFFCURVE:
 			firstPoint = thisPath.nodes[-1] # closed path that starts with a curve
-		dotPoints = [ firstPoint ]
-		fineGrainPoints = getFineGrainPointsForPath( thisPath, distanceBetweenDots )
+		dotPoints = [firstPoint]
+		fineGrainPoints, fineGrainTangentAngles = getFineGrainPointsForPath(thisPath, distanceBetweenDots)
+		tangentAngles = [fineGrainTangentAngles[0]]
 	
 		myLastPoint = dotPoints[-1]
-	
-		for thisPoint in fineGrainPoints:
-			if distance( myLastPoint, thisPoint ) >= distanceBetweenDots:
-				dotPoints += [thisPoint]
+		for i, thisPoint in enumerate(fineGrainPoints):
+			if distance(myLastPoint, thisPoint) >= distanceBetweenDots:
+				dotPoints.append(thisPoint)
+				tangentAngles.append(fineGrainTangentAngles[i])
 				myLastPoint = thisPoint
 			else:
 				pass
@@ -155,24 +199,29 @@ def dotCoordsOnPath( thisPath, distanceBetweenDots, balanceOverCompletePath=Fals
 		if balanceOverCompletePath:
 			reversePath = thisPath.copy()
 			reversePath.reverse()
-			reverseDotPoints = dotCoordsOnPath( reversePath, distanceBetweenDots )
-			numberOfPoints = min( len(reverseDotPoints), len(dotPoints)-1 )
+			reverseDotPoints, reverseTangentAngles = dotCoordsOnPath(reversePath, distanceBetweenDots)
+			numberOfPoints = min(len(reverseDotPoints), len(dotPoints)-1)
 			for i in range(numberOfPoints):
 				factor = 1.0/numberOfPoints*i
 				j = -1-i
-				newPos = interpolatePointPos( dotPoints[j], reverseDotPoints[i], factor )
+				newPos = interpolatePointPos(dotPoints[j], reverseDotPoints[i], factor)
 				dotPoints[j] = newPos
+
+				newAngle = tangentAngles[j] * (1-factor) + reverseTangentAngles[i] * factor
+				tangentAngles[j] = newAngle
 		
 		if distance(dotPoints[0], dotPoints[-1]) < 0.8: #closed path
 			dotPoints.pop(-1)
+			tangentAngles.pop(-1)
 			
-		return dotPoints
+		return dotPoints, tangentAngles
 	except Exception as e:
 		import traceback
 		print(traceback.format_exc())
 
+
 @objc.python_method
-def isSelected( thisPath ):
+def isSelected(thisPath):
 	if thisPath:
 		thisLayer = thisPath.parent
 		if thisLayer:
@@ -181,12 +230,13 @@ def isSelected( thisPath ):
 					return True
 	return False
 
+
 @objc.python_method
-def placeDots( thisLayer, useBackground, componentNameString, distanceBetweenDots, balanceOverCompletePath=False, selectionMatters=False, deleteComponents=False ):
+def placeDots(thisLayer, useBackground, componentNameString, distanceBetweenDots, balanceOverCompletePath=False, selectionMatters=False, deleteComponents=False):
 	try:
 		# find out component offset:
-		xOffsets = []
-		yOffsets = []
+		xOffsets, yOffsets = [], []
+		componentAngles = []
 		thisFont = thisLayer.parent.parent
 		componentNames = [name.strip(" *") for name in componentNameString.split(",") if thisFont.glyphs[name.strip(" *")]]
 		shouldUseMask = ["*" in name for name in componentNameString.split(",")]
@@ -194,13 +244,24 @@ def placeDots( thisLayer, useBackground, componentNameString, distanceBetweenDot
 		
 		if sourceComponents:
 			for sourceComponent in sourceComponents:
-				sourceAnchor = sourceComponent.layers[thisLayer.associatedMasterId].anchors["origin"]
+				sourceLayer = sourceComponent.layers[thisLayer.associatedMasterId]
+				
+				# if component has an origin anchor, record the offset:
+				sourceAnchor = sourceLayer.anchors["origin"]
 				if sourceAnchor:
-					xOffsets.append(-sourceAnchor.position.x)
-					yOffsets.append(-sourceAnchor.position.y)
+					originPosition = sourceAnchor.position
 				else:
-					xOffsets.append(0)
-					yOffsets.append(0)
+					originPosition = NSPoint(0, 0)
+				xOffsets.append(-originPosition.x)
+				yOffsets.append(-originPosition.y)
+				
+				# if component has an orientation anchor, make note of it:
+				orientationAnchor = sourceLayer.anchors["orientation"]
+				if orientationAnchor:
+					angle = getOrientationAngle(originPosition, orientationAnchor.position)
+					componentAngles.append(angle)
+				else:
+					componentAngles.append(None)
 		
 			# use background if specified:
 			if useBackground:
@@ -216,12 +277,12 @@ def placeDots( thisLayer, useBackground, componentNameString, distanceBetweenDot
 			selectedPathHashes = []
 			if deleteComponents:
 				if not selectionMatters:
-					if not deleteAllComponents( thisLayer ):
+					if not deleteAllComponents(thisLayer):
 						print("-- Error deleting previously placed components.")
 				else:
 					for thisPath in sourceLayer.paths:
 						if isSelected(thisPath):
-							selectedPathHashes.append( thisPath.__hash__() )
+							selectedPathHashes.append(thisPath.__hash__())
 					if selectedPathHashes:
 						for i in reversed(range(len(thisLayer.shapes))):
 							if not isinstance(thisLayer.shapes[i], GSComponent):
@@ -235,7 +296,8 @@ def placeDots( thisLayer, useBackground, componentNameString, distanceBetweenDot
 				pathHash = thisPath.__hash__()
 				pathIsSelected = pathHash in selectedPathHashes
 				if not selectionMatters or pathIsSelected:
-					for i, thisPoint in enumerate(dotCoordsOnPath( thisPath, distanceBetweenDots, balanceOverCompletePath )):
+					dotsOnPath, anglesOnPath = dotCoordsOnPath(thisPath, distanceBetweenDots, balanceOverCompletePath)
+					for i, thisPoint in enumerate(dotsOnPath):
 						j = i % len(sourceComponents)
 						newComp = GSComponent(
 							componentNames[j],
@@ -247,9 +309,13 @@ def placeDots( thisLayer, useBackground, componentNameString, distanceBetweenDot
 						newComp.alignment = -1
 						if shouldUseMask[j]:
 							newComp.attributes["mask"] = 1
-						thisLayer.addShape_( newComp )
+						
+						if componentAngles[j] is not None:
+							rotateAngle = anglesOnPath[i] - componentAngles[j]
+							newComp.applyTransform(createTransformMatrix(thisPoint, rotateAngle))
+						
+						thisLayer.addShape_(newComp)
 						newComp.setUserData_forKey_(pathHash, "originPath")
-				
 			return True
 		else:
 			return False
@@ -260,10 +326,11 @@ def placeDots( thisLayer, useBackground, componentNameString, distanceBetweenDot
 		print(traceback.format_exc())
 		return False
 
+
 @objc.python_method
-def minimumOfOne( value ):
+def minimumOfOne(value):
 	try:
-		returnValue = float( value )
+		returnValue = float(value)
 		if returnValue < 1.0:
 			returnValue = 1.0
 	except:
@@ -271,18 +338,19 @@ def minimumOfOne( value ):
 		
 	return returnValue
 
+
 @objc.python_method
-def process( thisLayer, deleteComponents, componentName, distanceBetweenDots, useBackground=True, balanceOverCompletePath=False, selectionMatters=False ):
+def process(thisLayer, deleteComponents, componentName, distanceBetweenDots, useBackground=True, balanceOverCompletePath=False, selectionMatters=False):
 	try:
-		if useBackground and len( thisLayer.paths ) > 0:
+		if useBackground and len(thisLayer.paths) > 0:
 			if thisLayer.className() == "GSBackgroundLayer":
 				thisLayer = thisLayer.foreground()
 			thisLayer.background.clear()
 			for thisPath in thisLayer.paths:
 				try:
-					thisLayer.background.shapes.append( thisPath.copy() )
+					thisLayer.background.shapes.append(thisPath.copy())
 				except:
-					thisLayer.background.paths.append( thisPath.copy() )
+					thisLayer.background.paths.append(thisPath.copy())
 			
 			try:
 				for i in reversed(range(len(thisLayer.shapes))):
@@ -292,12 +360,13 @@ def process( thisLayer, deleteComponents, componentName, distanceBetweenDots, us
 			except:
 				thisLayer.paths = None
 	
-		if not placeDots( thisLayer, useBackground, componentName, distanceBetweenDots, balanceOverCompletePath, selectionMatters, deleteComponents ):
+		if not placeDots(thisLayer, useBackground, componentName, distanceBetweenDots, balanceOverCompletePath, selectionMatters, deleteComponents):
 			print("-- Could not place components at intervals of %.1f units." % distanceBetweenDots)
 	except Exception as e:
 		print("Stitcher Error:\n%s"%e)
 		import traceback
 		print(traceback.format_exc())
+
 
 class Stitcher(FilterWithDialog):
 	# Definitions of IBOutlets
@@ -341,42 +410,42 @@ class Stitcher(FilterWithDialog):
 	def start(self):
 		
 		# Set default value
-		Glyphs.registerDefault( self.domain('interval'), 100.0 )
-		Glyphs.registerDefault( self.domain('component'), "_circle" )
-		Glyphs.registerDefault( self.domain('balance'), 0)
+		Glyphs.registerDefault(self.domain('interval'), 100.0)
+		Glyphs.registerDefault(self.domain('component'), "_circle")
+		Glyphs.registerDefault(self.domain('balance'), 0)
 		
 		# Set value of text field
-		self.intervalField.setStringValue_( self.pref('interval') )
-		self.componentField.setStringValue_( self.pref('component').strip() )
-		self.balanceCheckbox.setIntValue_( bool(self.pref('balance')) )
+		self.intervalField.setStringValue_(self.pref('interval'))
+		self.componentField.setStringValue_(self.pref('component').strip())
+		self.balanceCheckbox.setIntValue_(bool(self.pref('balance')))
 		
 		# Set focus to text field
 		self.intervalField.becomeFirstResponder()
 		
 	# Action triggered by UI
 	@objc.IBAction
-	def setInterval_( self, sender ):
+	def setInterval_(self, sender):
 		# Store value coming in from dialog
 		self.setPref('interval', sender.floatValue())
 		self.update()
 
 	# Action triggered by UI
 	@objc.IBAction
-	def setComponent_( self, sender ):
+	def setComponent_(self, sender):
 		# Store value coming in from dialog
 		self.setPref('component', sender.stringValue().strip())
 		self.update()
 
 	# Action triggered by UI
 	@objc.IBAction
-	def setBalance_( self, sender ):
+	def setBalance_(self, sender):
 		# Store value coming in from dialog
 		self.setPref('balance', sender.intValue())
 		self.update()
 	
 	# Action triggered by UI
 	@objc.IBAction
-	def setUseBackground_( self, sender ):
+	def setUseBackground_(self, sender):
 		# Store value coming in from dialog
 		self.setPref('useBackground', sender.intValue())
 		self.update()
@@ -422,14 +491,14 @@ class Stitcher(FilterWithDialog):
 					else:
 						if interval and component:
 							# settings:
-							distanceBetweenDots = minimumOfOne( interval )
-							balanceOverCompletePath = bool( self.pref("balance") )
+							distanceBetweenDots = minimumOfOne(interval)
+							balanceOverCompletePath = bool(self.pref("balance"))
 							if customParameters:
 								useBackground = False
 								deleteComponents = False
 								selectedLayers = (layer,)
 							else:
-								useBackground = bool( self.pref("useBackground") )
+								useBackground = bool(self.pref("useBackground"))
 								deleteComponents = True
 								selectedLayers = Font.selectedLayers
 								if len(selectedLayers) == 1 and inEditView:
@@ -461,8 +530,8 @@ class Stitcher(FilterWithDialog):
 			print(traceback.format_exc())
 		
 	@objc.python_method
-	def generateCustomParameter( self ):
-		return "%s; component:%s; interval:%s; balance:%s" % ( self.__class__.__name__, 
+	def generateCustomParameter(self):
+		return "%s; component:%s; interval:%s; balance:%s" % (self.__class__.__name__, 
 			self.pref('component'),
 			self.pref('interval'),
 			self.pref('balance'),
